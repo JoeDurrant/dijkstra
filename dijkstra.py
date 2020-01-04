b34a62e99a171316edf7d3a9fef4d192d978159c
JoeDurrant <josephwdurrant@gmail.com>
2020-01-04 23:34:20 +0000

    Implement path calculation into dijkstra
    - dijkstra() now returns a dictionary containing the shortest paths to
    all nodes, starting at target node and ending at source node

--- dijkstra.py
+++ dijkstra.py
@@ -2,12 +2,16 @@ from Graph import Graph
 def dijkstra(graph, source):
     distance = {}
     unvisited = {}
+    previous_node = {}
+
     for i in range(graph.get_number_of_vertices()):
         distance[i] = 1000
         unvisited[i] = 1000
+        previous_node[i] = -1 #Undefined at this point
     distance[source] = 0
     unvisited[source] = 0
-    
+    previous_node[source] = source
+
     while len(unvisited) > 0:
         current_node = min(unvisited, key=unvisited.get) # Should get ID number of source node on first loop
         unvisited.pop(current_node)
@@ -18,8 +22,19 @@ def dijkstra(graph, source):
                 if a < distance[neighbour]:
                     distance[neighbour] = a
                     unvisited[neighbour] = a
-    return distance
-    
+                    previous_node[neighbour] = current_node
+
+    paths = {}
+    #Calculate paths from source node to all nodes
+    for i in range(graph.get_number_of_vertices()):
+        target = i
+        paths[i] = []
+        while target != source:
+            paths[i].append(target)
+            target = previous_node[target] 
+        paths[i].append(source)
+    return distance, paths
+
 edges = [[0,0,0,0,1,4,0],
         [0,0,0,3,1,1,15],
         [0,0,0,6,0,0,3],
@@ -30,4 +45,6 @@ edges = [[0,0,0,0,1,4,0],
 
 graph1 = Graph(edges)
 
-print(dijkstra(graph1,1))
+distance, paths = dijkstra(graph1,1)
+print(distance)
+print(paths)
